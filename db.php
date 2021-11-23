82cc7a692e3318067b828196c687bf61747bbf1d
NagalieArt <70587195+NagalieArt@users.noreply.github.com>
2021-11-23 11:10:48 +0100

    Database, db.php, is added and connected to MySQL database: questa.

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+
+$host     = "localhost";
+$user     = "root";
+$password = "";
+$database = "questa";
+
+$db = mysqli_connect($host, $user, $password, $database) or die("Error: " . mysqli_connect_error());
+
+
